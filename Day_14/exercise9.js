711589e31071a42b3a1cd40bf1788c1e36282fa9
Jivan Sharma <jivan4sharma3@gmail.com>
2025-12-14 22:14:21 +0545

    Day 14 Completed

--- Day_14/exercise9.js
+++ Day_14/exercise9.js
@@ -1,13 +1,18 @@
 let num1 = prompt("Enter first number: ")
+let operation = prompt("Enter operation")
 let num2 = prompt("Enter second number: ")
 
-function rendom_num(){
-    let ran_num = parseInt(Math.random()*100)
-    return ran_num
+let random = Math.random() * 100
+
+let obj = {
+    "+": "-",
+    "-": "/",
+    "*": "+",
+    "/": "**",
 }
 
-let ranNum = rendom_num()
-
-function faultCal(){
-    
+if (random > 0.1) {
+    alert(`the result is ${eval(`${num1} ${operation} ${num2}`)}`)
+} else {
+    operation = obj[operation]
 }
