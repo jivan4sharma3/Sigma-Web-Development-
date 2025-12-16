bbccc4e245b25aaa86cfc3db4c73f5ee3747cb8c
Jivan Sharma <jivan4sharma3@gmail.com>
2025-12-16 23:02:45 +0545

    Day 15 Completed

---
+++ Day_15/dom.js
@@ -0,0 +1,5 @@
+console.log(document.body.childNodes)
+
+console.log(document.body.firstElementChild)
+console.log(document.body.firstElementChild.childNodes)
+console.log(document.body.firstElementChild.children)
