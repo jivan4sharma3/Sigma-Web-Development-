c1bac31751cb046971bc9cba7dd13bd5c4965d70
Jivan Sharma <jivan4sharma3@gmail.com>
2025-12-25 21:26:23 +0545

    Day 25 completed

--- Day_25/server.js
+++ Day_25/server.js
@@ -1,8 +1,25 @@
-var slugify = require('slugify')
+// var slugify = require('slugify')
 
-let a = slugify('some string') // some-string
-console.log(a)
+// let a = slugify('some string') // some-string
+// console.log(a)
 
 // if you prefer something other than '-' as separator
-let b = slugify('some string', '_')  // some_string
-console.log(b)
+// let b = slugify('some string', '_')  // some_string
+// console.log(b)
+
+
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('<h1> Hello World </h1>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+ 
