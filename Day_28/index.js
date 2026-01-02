fe026607aca0125bf45743250f93b06c725bc8c0
Jivan Sharma <jivan4sharma3@gmail.com>
2026-01-02 22:29:49 +0545

    Day 28 Completed

--- Day_28/index.js
+++ Day_28/index.js
@@ -4,28 +4,26 @@ const port = 3000
 
 app.use(express.static("public"))
 
-// this is middleware.
+// middleware 
 app.use((req,res,next)=>{
     console.log("LOGGED")
     next()
 })
 
-app.use((req,res,next)=>{
-    console.log("LOGGED 2")
-    next()
-})
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
+
 })
 
 app.get('/about', (req, res) => {
-  res.send('Hello World in about route!')
+  res.send('Hello World! at about route')
 })
 
+
 app.get('/contact', (req, res) => {
-  res.send('Hello World! in contact route !')
+  res.send('Hello World! at contact route')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
