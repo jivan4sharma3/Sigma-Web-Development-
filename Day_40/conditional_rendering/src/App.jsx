c5dbba72eebda2c127787e2e94855225c227ed55
Jivan Sharma <jivan4sharma3@gmail.com>
2026-01-12 23:08:54 +0545

    Day 40 Completed

---
+++ Day_40/conditional_rendering/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [show, setShow] = useState(true)
+
+  const [todos, setTodos] = useState([
+    {
+      title: "Hey",
+      desc: "I am a good todo"
+    },
+    {
+      title: "Hey Another todo",
+      desc: "I am a good todo too"
+    },
+    {
+      title: "Hey I am grocery todo",
+      desc: "I am a good todo but I am grocery todo"
+    },
+
+  ])
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      {/* {show ? <button>This a btn when show is true </button> : ""} */}
+
+      {todos.map(todo => {
+        // return <Todo key={todo.title} todo={todo}/>
+        return <div key={todo.title} className="m-4 border border-1 border-purple-400">
+
+          <div className="todo">{todo.title}</div>
+          <div className="todo">{todo.desc}</div>
+        </div>
+      })}
+      {show && <button>This a btn when show is true </button>}
+      <div className="card">
+
+        <button onClick={() => setShow(!show)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
