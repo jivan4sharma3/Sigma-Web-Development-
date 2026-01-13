9d394dcb65dd0d29bc296e501b1454a172699918
Jivan Sharma <jivan4sharma3@gmail.com>
2026-01-13 21:47:08 +0545

    Day 41 Completed

--- Day_41/src/App.jsx
+++ Day_41/src/App.jsx
@@ -5,32 +5,42 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [name, setName] = useState("Jivan")
+  // const [name, setName] = useState("Jivan")
+  const [form, setForm] = useState({
+    name: "",
+    phone: ""
+  })
 
 
-  const  handleClick= ()=>{
+  const handleClick = () => {
     alert("the button was clicked")
   }
 
-  const handelMouseover = ()=>{
+  const handelMouseover = () => {
     alert("the Event was MouseOver Event ")
   }
 
-  const handleOnchange = (e)=>{
-    setName(e.target.value)
+  const handleOnchange = (e) => {
+    // // setName(e.target.value)
+    // setForm(...form, [e.traget.name]: e.traget.value)
   }
 
   return (
     <>
-     <div>
-         <button onClick={handleClick}>Click Me </button>
-     </div>
-     {/* <div className="red-div" onMouseOver={handelMouseover}>
+      <div>
+        <button onClick={handleClick}>Click Me </button>
+      </div>
+      {/* <div className="red-div" onMouseOver={handelMouseover}>
 
      </div> */}
-     <div>
-      <input type="text" value={name} onChange={handleOnchange} />
-     </div>
+      <div>
+        <input type="text" value={name} onChange={handleOnchange} />
+      </div>
+
+      <div>
+        <input type="text" value={form.name} name='name' onChange={handleOnchange} />
+        <input type="text" value={form.phone} name='phone' onChange={handleOnchange} />
+      </div>
     </>
   )
 }
