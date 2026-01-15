f52413e295e86a14a01450a07bda409102ebb025
Jivan Sharma <jivan4sharma3@gmail.com>
2026-01-15 20:22:57 +0545

    Day 43 Completed

--- Day_43/react-router/src/components/Navbar.jsx
+++ Day_43/react-router/src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li> 
-                <li>Contact</li>
-            </ul>
-        </nav>
-      
-    </div>
+      <nav>
+        <ul>
+          <li><NavLink className={(e) => { return e.isActive ? "red" : "" }} to="/">Home</NavLink></li>
+          <li><NavLink className={(e) => { return e.isActive ? "red" : "" }} to="/about">About </NavLink></li>
+          <li><NavLink className={(e) => { return e.isActive ? "red" : "" }} to="/contact">Contact</NavLink></li>
+          <li><NavLink className={(e) => { return e.isActive ? "red" : "" }} to="/login">Login</NavLink></li>
+        </ul >
+      </nav >
+
+    </div >
   )
 }
 
