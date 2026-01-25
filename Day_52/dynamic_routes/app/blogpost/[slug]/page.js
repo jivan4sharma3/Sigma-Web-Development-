6b00125de13e2978edcccb46ba89ad114cdc3c26
Jivan Sharma <jivan4sharma3@gmail.com>
2026-01-25 22:47:35 +0545

    Day 52 Completed

--- Day_52/dynamic_routes/app/blogpost/[slug]/page.js
+++ Day_52/dynamic_routes/app/blogpost/[slug]/page.js
@@ -1,12 +1,3 @@
-export default async function Page({ params }) {
-    const { slug } = await params
-    let languages = ["python", "c", "javaScript", "java"]
-    if (languages.includes(params.slug)) {
-        return <div>My Post: {slug}</div>
-    }
-    else {
-        return <div>Page is not found</div>
-
-    }
-    return <div>My Post: {slug}</div>
+export default function Page({params}) {
+   return <div>MY Post : {params.slug} </div>
 }
